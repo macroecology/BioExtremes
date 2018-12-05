85472ea6560bc8ceaddfb6c0b8b5611c2c40e4eb
ajpelu <ajpelu@gmail.com>
2018-12-05 10:18:59 +0100

    add code to interpolate function

--- interpolate.R
+++ interpolate.R
@@ -3,6 +3,34 @@
 ## no data, or ocean when interpolating land)
 
 
+interpolate <- function(anomaly, grid, method=spline,...) { 
+    require(tidyr)
+    
+    # check anomaly vector  
+    if((!is.vector(anomaly))) { 
+        anomaly <- as.vector(anomaly)
+    }
+    
+    # Remove rows with NA in anomaly 
+    if (!all(is.na(anomaly))) {
+        message("Warning: \n Rows with NA values in anomaly were removed")
+        anomaly <- drop_na(anomaly)
+    }
+    
+    # Checks grid 
+    if (!(is.matrix(grid) | is.data.frame(grid))) {
+        stop("A matrix or dataframe with two colums should be supplied")
+    } 
+    if (length(grid) < 2){
+        stop("Grid object must contain at least two colums (coordinates)")
+    }
+    
+    # Checks interpolate method 
+    method(anomaly, grid, ...)
+    
+} 
+
+
 spline <- function(x, grid, ...) {
     # Wrapper function to interpolate with thin plate splines
     # 'x' is a data.frame with 'Anom,X,Y values'
