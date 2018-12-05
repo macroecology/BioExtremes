1d327a4618377b2a0e93063caa9c39118bd3c73c
Johan Renaudie <johan.renaudie@mfn.berlin>
2018-12-05 15:15:10 +0100

    added upload function

--- connect_to_tfp.R
+++ connect_to_tfp.R
@@ -3,51 +3,44 @@ foldername<- "anomalies_land"
 
 download ("present_baseline", "present_baseline")
 
-## both parameters are character 
+## both parameters are character
 
 download<- function (foldername, saving_folder){
-  
-library(config)
-ftp_host = config::get("ftp")$host
-ftp_user = config::get("ftp")$username
-ftp_password = config::get("ftp")$password
 
-options(timeout=300)
-library(RCurl)
-url <- paste (ftp_host, "/ftpuser0063/", sep="")
-userpwd <- paste (ftp_user, ftp_password, sep=":")
+  library(config)
+  ftp_host = config::get("ftp")$host
+  ftp_user = config::get("ftp")$username
+  ftp_password = config::get("ftp")$password
 
-filenames <- getURL(paste0(url, foldername, "/"),
-                    userpwd = userpwd,
-                    ftp.use.epsv = FALSE, dirlistonly=TRUE) 
+  options(timeout=300)
+  library(RCurl)
+  url <- paste (ftp_host, "/ftpuser0063/", sep="")
+  userpwd <- paste (ftp_user, ftp_password, sep=":")
 
-filenames <- strsplit(filenames, '\n')
-filenames <- unlist(filenames)
-filenames <- filenames [-c(1:2)]
-# adjust foldernames, currently set to hackthon variables
-dir.create (paste0(getwd(), "/data/", saving_folder))
-for (filename in filenames) {
-  bin <- getBinaryURL(paste0(url, foldername, "/", filename),
-                      userpwd=userpwd) 
-#  if(any(grepl(".nc", dir()))){
-    writeBin(bin, paste0(getwd(), "/data/", saving_folder, "/", filename))
-#  }
+  filenames <- getURL(paste0(url, foldername, "/"),
+                      userpwd = userpwd,
+                      ftp.use.epsv = FALSE, dirlistonly=TRUE)
+
+  filenames <- strsplit(filenames, '\n')
+  filenames <- unlist(filenames)
+  filenames <- filenames [-c(1:2)]
+  # adjust foldernames, currently set to hackthon variables
+  dir.create (paste0(getwd(), "/data/", saving_folder))
+  for (filename in filenames) {
+    bin <- getBinaryURL(paste0(url, foldername, "/", filename),
+                        userpwd=userpwd)
+  #  if(any(grepl(".nc", dir()))){
+      writeBin(bin, paste0(getwd(), "/data/", saving_folder, "/", filename))
+  #  }
+  }
 }
-}
 
-
-
-
-#create dummy file for test upload
-upload.file <- "dummy"
-write(upload.file, file="test_clim_dat.txt")
-write(upload.file, file="test_clim_dat2.txt")
-
-# adjust regex to unique filename characters
-upload.files <- dir()[grep("clim_dat", dir())]
-
-for (filename in upload.files) {
-  ftpUpload(I(filename), 
-            paste0("ftp://", userpwd, "@", "ftp.naturkundemuseum-berlin.de/",
-                   ftp_user, "/", foldernames[grep("hackathon", foldernames)], "/", filename))
+upload <- function(origin_file, destination_file){
+  library(config)
+  library(RCurl)
+  ftp_host = config::get("ftp")$host
+  ftp_user = config::get("ftp")$username
+  ftp_password = config::get("ftp")$password
+  options(timeout=300)
+  ftpUpload(I(origin_file), paste0("ftp://", userpwd, "@", gsub("^s?ftp://",ftp_host), ftp_user, "/", destination_file))
 }
