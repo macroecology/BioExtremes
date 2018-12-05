58d199223229e9d005094acf8c7b3831fff35167
Pedro Tarroso <ptarroso@cibio.up.pt>
2018-12-05 11:20:40 +0000

    Interpolation working

--- interpolate.R
+++ interpolate.R
@@ -2,44 +2,52 @@
 ## package but it does allow to control where interpolation is made (avoid
 ## no data, or ocean when interpolating land)
 
+## Example use with decimal degrees lat an lon:
+## interpolate(anom, grid, lon.lat=T)
 
-interpolate <- function(anomaly, grid, method=spline,...) { 
-    require(tidyr)
-    
-    # check anomaly vector  
-    if((!is.vector(anomaly))) { 
-        anomaly <- as.vector(anomaly)
+interpolate <- function(anomaly, grid, method=spline, ...) { 
+    # It receives a raster stack, interpolates and returns a matrix
+    # Anomaly - raster stack with nlayers as nDays
+    # grid - X,Y defining interpolation locations
+    # method - Interpolation function (default is spline)
+
+    # Check if anomaly is a raster stack
+    if (!("RasterStack" %in% class(anomaly))) {
+        stop("Anomaly must be a raster stack")
     }
-    
-    # Remove rows with NA in anomaly 
-    if (!all(is.na(anomaly))) {
-        message("Warning: \n Rows with NA values in anomaly were removed")
-        anomaly <- drop_na(anomaly)
-    }
-    
+
     # Checks grid 
     if (!(is.matrix(grid) | is.data.frame(grid))) {
         stop("A matrix or dataframe with two colums should be supplied")
     } 
-    if (length(grid) < 2){
-        stop("Grid object must contain at least two colums (coordinates)")
+
+    anom.grid <- coordinates(anomaly)
+    n <- nlayers(anomaly)
+
+    # Matrix to store interpolation results
+    intpl <- matrix(NA, nrow(grid), n+2)
+    intpl[,1:2] <- as.matrix(grid)
+
+    for (i in 1:n) {
+        anom.values <- anomaly[[i]][]
+        dt.mask <- !is.na(anom.values)
+        x <- anom.values[dt.mask]
+        x.grid <- anom.grid[dt.mask,]
+        intpl[,i+2] <- method(x, x.grid, grid)
     }
-    
-    # Checks interpolate method 
-    method(anomaly, grid, ...)
-    
+    colnames(intpl) <- c(colnames(grid), names(anomaly))
+    intpl    
 } 
 
 
-spline <- function(x, grid, ...) {
+spline <- function(x, original, target, ...) {
     # Wrapper function to interpolate with thin plate splines
-    # 'x' is a data.frame with 'X,Y,Anom' values
-    # 'grid' is a data frame with 'X,Y' values to interpolate
+    # 'x' is a vector with values to interpolate
+    # 'original' is a grid (X,Y) with coordinates locations of x
+    # 'target' is a grid (X,Y) with locations to interpolate into
     # '...' extra arguments to pass to Tps function (lon.lat=T is useful)
     require(fields)
-    grd <- x[,1:2]
-    val <- x[,3]
-    fit <- Tps(grd, val, ...)
-    predict(fit, grid)[,1]
+    fit <- Tps(original, x, ...)
+    predict(fit, target)[,1]
 }
     
