0e25c67077df9dc4fc70b1caece27b9b8bea1cc1
plannapus <johan.renaudie@mfn-berlin.de>
2018-12-05 14:08:38 +0100

    split the script

--- wrapper_2_combine_baseline+anomaly.R
+++ wrapper_2_combine_baseline+anomaly.R
@@ -1,4 +1,4 @@
-wrapper <- function(baseline_name, variable_file, function_file, output_name){
+wrapper <- function(baseline_name, anomaly_file, land_or_ocean="land", output_name){
   # Name of the baseline file
   # Name of the variable file
   # Name of the file to the function to apply
@@ -38,43 +38,36 @@ wrapper <- function(baseline_name, variable_file, function_file, output_name){
   filenames <- strsplit(filenames, '\n')
   filenames <- unlist(filenames)
   filenames <- filenames[!grepl("^[.]+$",filenames)]
-  if(variable_file%in%filenames){
-    filename = filenames[filenames==variable_file]
+  if(anomaly_file%in%filenames){
+    filename = filenames[filenames==anomaly_file]
   }else{
-    stop("Variable file name does not exist on the FTP server")
+    stop("Anomaly file name does not exist on the FTP server")
   }
   dir.create("data/variables")
-  for (filename in filenames) {
-    bin <- getBinaryURL(paste0(url, foldernames[grep("hackathon", foldernames)], "/", filename), userpwd=userpwd)
-    writeBin(bin, paste0("/data/variables/", filename))
-  }
+  bin <- getBinaryURL(paste0(url, foldernames[grep("hackathon", foldernames)], "/", filename), userpwd=userpwd)
+  writeBin(bin, paste0("/data/variables/", filename))
 
   #Remap baseline on regular grid
   datafiles = dir("data",full.names=TRUE,recursive=TRUE)
   baseline = datafiles[grepl(baseline_file,datafiles)]
   source("masking.R")
-  l <- mask(0.5,"land")
+  l <- mask(0.5,land_or_ocean)
   base = raster(baseline)
   base_regrid = resample(base, l)
   base_masked = raster::mask(base_regrid)
 
-  #Mask and Interpolate data
+  anomaly = datafiles[grepl(anomaly_file,datafiles)]
+  ano = raster(baseline)
+  ano_regrid = resample(ano, l)
+  ano_masked = raster::mask(ano_regrid)
 
+  #Add anomaly to baseline
+  stack = stack(base_masked, ano_masked)
+  result = calc(stack, sum)
 
-
-  #Add to baseline
-  #(at this point let's say the XY365t output is called variable)
-
-  #Apply function
-  e=new.env()
-  source(function_file, local=e) #We can t assume the name of the function inside
-  func=base::get(ls(env=e),env=e) #So we do witchcraft to get it
-  output = data.frame(x=variable$x,y=variable$y)
-  output$fd = apply(variable[,-(1:2)],1,func)
-  output_rl = rasterfromXYZ(output)
-  dir.create("output")
-  writeRaster(output_rl, paste0("output/",output_name),"netCDF")
+  #sAVE THE FILE
+  writeRaster(result, paste0("output/",output_name),"netCDF")
   #Load output on ftp server
-  ftpUpload(I("output/frost_days.nc"), paste0("ftp://", userpwd, "@", "ftp.naturkundemuseum-berlin.de/", ftp_user, "/", foldernames[grep("hackathon", foldernames)], "/", filename))
+  ftpUpload(I("output/frost_days.nc"), paste0("ftp://", userpwd, "@", gsub("^s?ftp://",ftp_host), ftp_user, "/", foldernames[grep("hackathon", foldernames)], "/", filename))
 
 }
