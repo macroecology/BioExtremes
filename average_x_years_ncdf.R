7f736b657914583eeea89e9b9cce77879cd7c688
plannapus <johan.renaudie@mfn-berlin.de>
2018-12-06 12:35:39 +0100

    first draft of average_x_years_ncdf

--- average_x_years_ncdf.R
+++ average_x_years_ncdf.R
@@ -1,8 +1,32 @@
 average_x_years_ncdf <- function(ftp_folder,
                                  year_beginning, year_end,
                                  output_file){
+  # Function to make the baseline based on content of ftp folder
+  # for a specified year range
+
+  #Download folder
   source("download_from_ftp.R")
   saving_folder <- paste0("data/",ftp_folder)
   download(ftp_folder,saving_folder)
 
+  begin <- as.Date(paste0(year_beginning,"-01-01"))
+  end <- as.Date(paste0(year_end,"-12-31"))
+  #Read all files
+  all_files <- list()
+  result <- list()
+  files <- dir(saving_folder)
+  for(i in seq_along(files)){
+    r <- stack(files[i])
+    d <- as.Date(gsub("^X","",names(r)), tryFormats = c("%Y-%m-%d", "%Y/%m/%d","%Y.%m.%d"))  #Can we assume date to be in one of those formats?
+    r <- r[[d>=begin & d<=end]]           #Get rid of dates outside range
+    r <- r[[format(d,"%m.%d")!="02.29"]]  #Get rid of 29th of february if any
+    if(nlayers(r)>0) all_files[[i]] <- r
+  }
+  for(j in 1:365){
+    s <- stack(lapply(all_files,function(x), x[[j]]))
+    result[[j]] <- calc(s, mean)
+  }
+  out <- stack(result)
+  writeRaster(result, paste0("data/",output_file),"netCDF")
+  upload(output_file, ftp_folder, output_file) #where do we want to save it?
 }
