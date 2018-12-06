d33a98442f5e2dae342b7a41d9be5fff7a046b56
plannapus <johan.renaudie@mfn-berlin.de>
2018-12-06 15:41:26 +0100

    finished apply_extreme_function

--- apply_extreme_function.R
+++ apply_extreme_function.R
@@ -3,36 +3,26 @@ applyExtreme <- function(variable_file, function_file, output_name, ftp_director
   # Name of the variable file
   # Name of the file to the function to apply
   # Name of the output file
-
-  #Download baselines
-  source("upload_to_ftp.R")
-  library(config)
-  library(RCurl)
-  library(raster)
-  ftp_host <- config::get("ftp")$host
-  ftp_user <- config::get("ftp")$username
-  ftp_password <- config::get("ftp")$password
-  options(timeout=300)
-  url <- paste (ftp_host, "/ftpuser0063/", sep="")
-  userpwd <- paste (ftp_user, ftp_password, sep=":")
-  foldernames <- getURL(url, userpwd = userpwd, ftp.use.epsv = FALSE, dirlistonly=TRUE) # downloads folder names
-
-  foldernames <- strsplit(foldernames, '\n')
-  foldernames <- unlist(foldernames)
-  if(!dir.exists("data")) dir.create("data")
-  filenames <- getURL(paste0(url, foldernames[grep("hackathon", foldernames)], "/"), userpwd = userpwd, ftp.use.epsv = FALSE, dirlistonly=TRUE)
-  filenames <- strsplit(filenames, '\n')
-  filenames <- unlist(filenames)
-  filenames <- filenames[!grepl("^[.]+$",filenames)]
-  if(variable_file%in%filenames){
-    filename <- filenames[filenames==variable_file]
-  }else{
-    stop("Variable file name does not exist on the FTP server")
+  mat2xyz <- function(mat, lon, lat){
+    #Takes a variable matrix and 2 dimensions vectors and output a XYZ dataframe
+    lonlat = expand.grid(lon, lat)
+    sst = as.vector(mat)
+    data.frame(x=lonlat[,1], y=lonlat[,2], z=sst)
   }
-  if(!dir.exists("data/variables")) dir.create("data/variables")
-  bin <- getBinaryURL(paste0(url, foldernames[grep("hackathon", foldernames)], "/", filename), userpwd=userpwd)
-  writeBin(bin, paste0("/data/variables/", filename))
+  #Download baselines
+  require(raster)
+  source("upload_to_ftp.R")
+  download(variable_file,"/data/variables/")
 
+  r <- stack(paste0("/data/variables/",variable_file)
+  x <- rowFromCell(r[[1]],1:length(r[[1]]))
+  y <- colFromCell(r[[1]],1:length(r[[1]]))
+  variable <- cbind(x,y)
+  for(i in 1:365){
+    variable <- cbind(variable,as.vector(as.array(r[[i]])))
+  }
+  colnames(variable)<-c("x","y",1:365)
+  variable <- as.data.frame(variable)
   #Apply function
   e <- new.env()
   source(function_file, local=e) #We can t assume the name of the function inside,
