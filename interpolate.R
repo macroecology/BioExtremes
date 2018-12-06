8aeefc58a2c48d30ead3e144f1d1c98e83884650
Pedro Tarroso <ptarroso@cibio.up.pt>
2018-12-06 11:35:53 +0000

    Improved interpolation functions with idw and splines

--- interpolate.R
+++ interpolate.R
@@ -3,13 +3,14 @@
 ## no data, or ocean when interpolating land)
 
 ## Example use with decimal degrees lat an lon:
-## interpolate(anom, grid, lon.lat=T)
+## interpolate(anom, grid)
 
-interpolate <- function(anomaly, grid, method=spline, ...) { 
+clim.interpolate <- function(anomaly, grid, imethod=spline, use.direction=TRUE, ...) { 
     # It receives a raster stack, interpolates and returns a matrix
     # Anomaly - raster stack with nlayers as nDays
     # grid - X,Y defining interpolation locations
-    # method - Interpolation function (default is spline)
+    # imethod - Interpolation function (default is spline)
+    # use.direction - use cosine directions instead of true coordinates (spherical)
 
     # Check if anomaly is a raster stack
     if (!("RasterStack" %in% class(anomaly))) {
@@ -27,18 +28,33 @@ interpolate <- function(anomaly, grid, method=spline, ...) {
     # Matrix to store interpolation results
     intpl <- matrix(NA, nrow(grid), n+2)
     intpl[,1:2] <- as.matrix(grid)
+    colnames(intpl) <- c(colnames(grid), names(anomaly))
+
+    if (use.direction) {
+        anom.grid <- dircos(anom.grid) * 6371 # earth radius km
+        grid <- dircos(grid) * 6371  
+    }
 
     for (i in 1:n) {
         anom.values <- anomaly[[i]][]
         dt.mask <- !is.na(anom.values)
         x <- anom.values[dt.mask]
         x.grid <- anom.grid[dt.mask,]
-        intpl[,i+2] <- method(x, x.grid, grid)
+        intpl[,i+2] <- imethod(x, x.grid, grid, ...)
     }
-    colnames(intpl) <- c(colnames(grid), names(anomaly))
     intpl    
 } 
 
+dircos <- function(x1) {
+    # Calculates the directions of the coordinate vectors to approximate
+    # great circle distances (adapted from Tps package)
+    # x1 - is a table with (lon,lat) coordinates.
+    coslat1 <- cos((x1[, 2] * pi)/180)
+    sinlat1 <- sin((x1[, 2] * pi)/180)
+    coslon1 <- cos((x1[, 1] * pi)/180)
+    sinlon1 <- sin((x1[, 1] * pi)/180)
+    data.frame(a=coslon1*coslat1, b=sinlon1*coslat1, c=sinlat1)
+}
 
 spline <- function(x, original, target, ...) {
     # Wrapper function to interpolate with thin plate splines
@@ -51,3 +67,18 @@ spline <- function(x, original, target, ...) {
     predict(fit, target)[,1]
 }
     
+
+
+inv.dist.w <- function(x, original, target, ...) {
+    # Wrapper function to interpolate with inverse distance weights
+    # 'x' is a vector with values to interpolate
+    # 'original' is a grid (X,Y) with coordinates locations of x
+    # 'target' is a grid (X,Y) with locations to interpolate into
+    # '...' extra arguments to pass to idw function (nmax or maxdist)
+    require(gstat)
+    x <- data.frame(x)
+    coordinates(x) <- original
+    target <- SpatialPoints(target)
+    pred <- idw(x~1, x, target, ...)
+    pred$var1.pred
+}
