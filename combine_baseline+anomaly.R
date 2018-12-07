f7bced9ffa66f00c16b4c00d8fa2b4a8105a4445
plannapus <johan.renaudie@mfn-berlin.de>
2018-12-07 11:17:22 +0100

    progress

--- combine_baseline+anomaly.R
+++ combine_baseline+anomaly.R
@@ -19,12 +19,10 @@ addAnomalyToBaseline <- function(baseline_name, anomaly_file, land_or_ocean="lan
   source("masking.R")
   l <- create_mask(0.5,land_or_ocean)
   base <- raster(saving1)
-  base_regrid <- resample(base, l)
-  base_masked <- mask(base_regrid)
+  base_masked <- base*l #Is it how one masks?
 
   ano <- raster(saving2)
-  ano_regrid <- resample(ano, l)
-  ano_masked <- mask(ano_regrid)
+  ano_masked <- ano*l
 
   #Add anomaly to baseline
   stack <- stack(base_masked, ano_masked)
